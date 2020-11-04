fb8eb14a426f3280447b03e348c5512ef9c9ccf3
Eduardo Assuncao <cunhaeduardo1231@gmail.com>
2020-11-04 20:36:31 -0300

    add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello'})
-})
+app.get('/', (req, res) => res.json({ message: 'Hello' }));
 
-export default app
+export default app;
